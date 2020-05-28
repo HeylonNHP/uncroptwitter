97a1aab564407e74927f12370cf12b17bcf0d838
heylon <heylon96@hotmail.com>
2020-05-28 16:07:31 +1000

    Padded container increase size

--- uncroptwitter.user.js
+++ uncroptwitter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter uncrop
 // @namespace    http://tampermonkey.net/
-// @version      0.2.11
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://twitter.com/home
@@ -91,69 +91,9 @@
 
         processBgImageContainers(bgImageContainers);
         return;
-
-        var paddedTweetImageElements = document.getElementsByClassName("r-1adg3ll r-13qz1uu");
-
-        var elementsString = "";
-
-        alert(paddedTweetImageElements.length);
-
-        for(var i =0; i < paddedTweetImageElements.length; ++i){
-            var element = paddedTweetImageElements[i];
-            elementsString = elementsString + element + "<--\n";
-            //alert("hi" + i);
-            element.style.paddingBottom = '100%';
-
-            var w = element.clientWidth;
-            var h = element.clientHeight;
-
-            var maxDimension = -1;
-            if(w>h){maxDimension = w;}else if(h>w){maxDimension = h;}else {maxDimension = w;}
-            //alert(maxDimension + "w " + w + "h " + h);
-
-            //part 2
-            var imageContainingElement = element.parentElement;
-            var layer1 = imageContainingElement.getElementsByTagName("div")[1];
-            var layer2 = layer1.firstChild;
-
-            if(layer2 == null || true){
-
-
-            }else {
-                var bgImageElement = layer2.firstChild;
-                var imageElement = layer2.lastChild;
-
-                var setSize = "n";
-
-                var scaleFactorW = element.clientWidth/imageElement.naturalWidth;
-                var scaleFactorH = element.clientHeight/imageElement.naturalHeight;
-                var scaleFactor = 0;
-
-                if(scaleFactorW<scaleFactorH){
-                    scaleFactor = scaleFactorW;
-                }else{
-                    scaleFactor = scaleFactorH;
-                }
-
-                bgImageElement.style.backgroundSize = (imageElement.naturalWidth*scaleFactor) + 'px ' +
-                    (imageElement.naturalHeight*scaleFactor) + 'px';
-
-                /*alert(imageElement.naturalWidth + " " + imageElement.naturalHeight + "\n" +
-                    element.clientWidth + " " + element.clientHeight + "\n" + setSize);*/
-
-                layer2.style.marginTop = '0%';
-                layer2.style.marginBottom = '0%';
-                layer2.style.marginLeft = '0%';
-                layer2.style.marginRight = '0%';
-
-
-                //alert("done");
-            }
-        }
-        alert(elementsString);
     }
 
     // Your code here...
-    alert("go");
-    setInterval(processImages,10000);
+    alert("starting twitter uncrop");
+    setInterval(processImages,1000);
 })();
